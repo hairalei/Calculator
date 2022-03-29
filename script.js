3f9de42b46f1db298a6f7cc8608fd778f88c143e
hairalei <darhaii13@gmail.com>
2022-03-29 16:47:14 +0800

    Make the calculator work for two numbers

--- script.js
+++ script.js
@@ -11,20 +11,54 @@ const btnNum = document.querySelectorAll(".btn-num");
 const btnOperators = document.querySelectorAll(".btn-operations");
 const btnClear = document.querySelector(".btn-clear");
 const btnBackspace = document.querySelector(".btn-backspace");
+const btnEqual = document.querySelector(".btn-equal");
 
 let firstNum, secondNum, firstOperator, secondOperator;
 
 btnClear.addEventListener("click", function () {
   lineBottom.textContent = "0";
   lineTop.innerHTML = "&nbsp;";
+  firstNum = 0;
+  secondNum = 0;
+  firstOperator = "";
+  secondOperator = "";
 });
 
-for (let btn of btnNum) {
-  btn.addEventListener("click", function () {
-    if (lineBottom.textContent === "0") lineBottom.textContent = "";
-    lineBottom.textContent += `${btn.textContent}`;
+function appendNum() {
+  for (let btn of btnNum) {
+    btn.addEventListener("click", function () {
+      if (lineBottom.textContent === "0") lineBottom.textContent = "";
+      if (!firstNum) firstNum = btn.textContent;
+      else secondNum = btn.textContent;
+      lineBottom.textContent += `${btn.textContent}`;
+    });
+  }
+}
+
+function setOperator() {
+  for (let btn of btnOperators) {
+    btn.addEventListener("click", function () {
+      if (!firstOperator) firstOperator = btn.textContent;
+      if (lineBottom.textContent === "0") lineBottom.textContent = "";
+      lineBottom.textContent += `${btn.textContent}`;
+    });
+  }
+}
+
+function calculate() {
+  appendNum();
+  setOperator();
+  btnEqual.addEventListener("click", () => {
+    lineTop.textContent = lineBottom.textContent;
+    lineBottom.textContent = operate(firstOperator, firstNum, secondNum);
+    firstNum = lineBottom.textContent;
+    secondNum = 0;
+    firstOperator = "";
+    secondOperator = "";
   });
 }
+
+calculate();
 
 function add(x, y) {
   return x + y;
@@ -48,7 +82,7 @@ function operate(operation, x, y) {
   switch (operation) {
     case "+":
       return add(x, y);
-    case "-":
+    case "−":
       return subtract(x, y);
     case "*":
       return multiply(x, y);
