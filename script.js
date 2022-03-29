0c2e8dc355ae42ba1ea911e8aae49206319fef4d
hairalei <darhaii13@gmail.com>
2022-03-29 21:33:26 +0800

    Reset the code for the display

--- script.js
+++ script.js
@@ -14,9 +14,7 @@ const btnBackspace = document.querySelector(".btn-backspace");
 const btnEqual = document.querySelector(".btn-equal");
 
 let firstNum, secondNum, firstOperator, secondOperator;
-let isOperatorPressed = false;
 let num = 0;
-let operatorCount = 0;
 
 btnClear.addEventListener("click", function () {
   lineBottom.textContent = "0";
@@ -26,71 +24,7 @@ btnClear.addEventListener("click", function () {
   secondNum = 0;
   firstOperator = "";
   secondOperator = "";
-  operatorCount = 0;
-  isOperatorPressed = false;
 });
-
-function appendNum() {
-  for (let btn of btnNum) {
-    btn.addEventListener("click", function () {
-      if (lineBottom.textContent === "0") lineBottom.textContent = "";
-      num += btn.textContent;
-      lineBottom.textContent += `${btn.textContent}`;
-
-      if (firstNum && secondNum) lineBottom.textContent = `${btn.textContent}`;
-
-      // console.log(firstNum, secondNum, num);
-    });
-  }
-}
-
-function storeNum() {
-  !firstNum ? (firstNum = num) : (secondNum = num);
-  num = 0;
-}
-
-function setOperator() {
-  for (let btn of btnOperators) {
-    btn.addEventListener("click", function () {
-      operatorCount++;
-      storeNum();
-      if (!firstOperator) firstOperator = btn.textContent;
-      //   if (lineBottom.textContent === "0") lineBottom.textContent = "";
-      lineBottom.textContent += `${btn.textContent}`;
-
-      if (operatorCount >= 2) {
-        calculate();
-        lineBottom.textContent = firstNum;
-        lineBottom.textContent += `${btn.textContent}`;
-      }
-      // console.log(firstNum, secondNum, num, isOperatorPressed);
-    });
-  }
-}
-
-function calculator() {
-  appendNum();
-  setOperator();
-  btnEqual.addEventListener("click", (e) => {
-    // console.log(firstNum, secondNum, num, isOperatorPressed);
-    storeNum();
-
-    isOperatorPressed = true;
-    calculate();
-  });
-}
-
-function calculate() {
-  lineTop.textContent = lineBottom.textContent;
-  lineBottom.textContent = operate(firstOperator, firstNum, secondNum);
-  console.log(operate(firstOperator, firstNum, secondNum));
-  firstNum = lineBottom.textContent;
-  secondNum = 0;
-  firstOperator = "";
-  secondOperator = "";
-}
-
-calculator();
 
 function add(x, y) {
   return x + y;
